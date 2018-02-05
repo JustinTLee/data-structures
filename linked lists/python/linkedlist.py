fe34d31b22a2c95d69d85d14d9a9c3997dc35053
JLee <justinlee.gwu@gmail.com>
2018-02-04 20:18:36 -0500

    Add node and singleLinkedList classes along with test cases for methods

---
+++ linked lists/python/linkedlist.py
@@ -0,0 +1,162 @@
+# History:
+# 2018-02-04 - JL - create node class
+#                 - create singleLinkedList class
+#                 - create singleLinkedLIst methods: nNodes, printLinkedList, returnValue, appendNode, insertNode
+#                                                    popLinkedList, deleteNode, __reachLinkedListPos
+
+class node:
+	
+	def __init__(self, value):
+		self.value = value
+		self.nextNode = None
+		self.prevNode = None
+
+class singleLinkedList:
+
+	def __init__(self, value):
+		self.__startNode = node(value)
+		self.__nNodes = 1
+
+	@property
+	def nNodes(self):
+		nNodes = self.__nNodes
+		return nNodes
+
+	def printLinkedList(self):
+		# set current node and position to be beginning of linked list
+		currentNode = self.__startNode
+		currentPos = 1
+
+		nodeStr = ""
+
+		# loop through all nodes by printing out the value of the current node
+		# set current node to be the next node
+		while currentNode != None:
+			currentStr = "[ " + str(currentPos) + " | " + str(currentNode.value) + " ]-->"
+			nodeStr = nodeStr + currentStr
+
+			currentPos += 1
+			currentNode = currentNode.nextNode
+
+		nodeStr = nodeStr + "X"
+		return nodeStr
+
+	def returnValue(self, pos):
+		if (pos > self.__nNodes) | (pos < 1):
+			print("Supplied position not defined in linked list.")
+			return
+		else:
+			# loop through nodes until position supplied matches position in linked list
+			# return node value
+			currentNode, _ = self.__reachLinkedListPos(pos)
+
+			return currentNode.value
+
+	def appendNode(self, value):
+		# append a new node to the end of the linked list
+
+		# create a new node
+		newNode = node(value)
+
+		# loop through list until reach end of linked list
+		endNode, _ = self.__reachLinkedListPos(self.__nNodes)
+
+		# link last node to new node and increment number of nodes by 1
+		endNode.nextNode = newNode
+		self.__nNodes += 1
+
+		return
+
+	def insertNode(self, value, pos):
+		# create new node
+		newNode = node(value)
+
+		# if position = 1, add to beginning
+		# if 1 < position < nNodes, add to position
+		# if position >= nNodes, add to end
+		# if position is negative or zero, break the program
+		if pos == 1:
+			newNode.nextNode = self.__startNode
+			self.__startNode = newNode
+
+			# increment number of nodes by 1
+			self.__nNodes += 1
+
+		elif 1 < pos < self.__nNodes:
+			# get the current node in the position argument
+			currentNode, previousNode = self.__reachLinkedListPos(pos)
+
+			# insert new node that that position
+			previousNode.nextNode = newNode
+			newNode.nextNode = currentNode
+
+			# increment number of nodes by 1
+			self.__nNodes += 1
+
+		elif pos < 1:
+			print("Position cannot be less than 1")
+			return
+
+		else:
+			if pos > self.__nNodes:
+				print("Position to insert new node exceeds number of nodes in the linked list. Appending to end instead.")
+
+			self.appendNode(value)
+
+		return
+
+	def popLinkedList(self):
+		# reach end of linked list
+		currentNode = self.__startNode
+
+		while currentNode.nextNode != None:
+			previousNode = currentNode
+			currentNode = previousNode.nextNode
+
+		# remove connection between previous node and current node
+		# output current node's value
+		previousNode.nextNode = None
+		return currentNode.value
+
+	def deleteNode(self, pos):
+		# if position not defined in linked list, return error
+		# if position is first, delete start node
+		# if position is not first, delete node at position and set connections accordingly
+
+		if (pos > self.__nNodes) | (pos < 1):
+			print("Node position has to be defined ")
+			return
+
+		elif pos == 1:
+			currentNode, _ = self.__reachLinkedListPos(2)
+			self.__startNode = currentNode
+
+		else:
+			currentNode, previousNode =  self.__reachLinkedListPos(pos)
+
+			# if current node is end of linked list, set previous node's next node to be none
+			# else set previous node's next node to be current node's next node
+			if currentNode == None:
+				previousNode.nextNode = None
+
+			else:
+				_, nextNode =  self.__reachLinkedListPos(pos + 1)
+				previousNode.nextNode = currentNode.nextNode
+
+		# decrement number of nodes by 1
+		self.__nNodes -= 1
+		return
+
+	def __reachLinkedListPos(self, pos):
+		# start at beginning of linked list
+		currentPos = 1
+		currentNode = self.__startNode
+		previousNode = None
+
+		# loop through nodes until current node is equal to position to insert into
+		while currentPos != pos:
+			previousNode = currentNode
+			currentNode = previousNode.nextNode
+			currentPos += 1
+
+		return currentNode, previousNode
---
+++ linked lists/python/test_linkedlist.py
@@ -0,0 +1,147 @@
+# History:
+# 2018-02-04 - JL - created class tests for Node and singleLinkedList classes
+#                 - created test cases for singleLinkedLIst methods: nNodes, printLinkedList, returnValue,
+#                                                                    appendNode, insertNode, popLinkedList,
+#                                                                    deleteNode
+
+import unittest
+import linkedlist as ll
+
+class TestNode(unittest.TestCase):
+
+    def setUp(self):
+        self.testNode = ll.node(1)
+
+    def test_initNode(self):
+        self.assertEqual(self.testNode.value, 1)
+        self.assertEqual(self.testNode.nextNode, None)
+
+class TestSingleLinkedList(unittest.TestCase):
+
+    def setUp(self):
+        self.testSingleLinkedList = ll.singleLinkedList(1)
+
+    def test_printLinkedList(self):
+        result = self.testSingleLinkedList.printLinkedList()
+        self.assertEqual(result, "[ 1 | 1 ]-->X")
+
+    def test_appendNode(self):
+        self.testSingleLinkedList.appendNode(2)
+        result = self.testSingleLinkedList.printLinkedList()
+        self.assertEqual(result, "[ 1 | 1 ]-->[ 2 | 2 ]-->X")
+
+        self.testSingleLinkedList.appendNode(3)
+        result = self.testSingleLinkedList.printLinkedList()
+        self.assertEqual(result, "[ 1 | 1 ]-->[ 2 | 2 ]-->[ 3 | 3 ]-->X")
+
+    def test_nNodes(self):
+        self.assertEqual(self.testSingleLinkedList.nNodes, 1)
+
+        self.testSingleLinkedList.appendNode(2)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 2)
+
+    def test_returnValue(self):
+        for k in range(2, 10 + 1):
+            self.testSingleLinkedList.appendNode(k)
+
+        self.assertEqual(self.testSingleLinkedList.returnValue(7), 7)
+        self.assertEqual(self.testSingleLinkedList.returnValue(11), None)
+        self.assertEqual(self.testSingleLinkedList.returnValue(0), None)
+
+    def test_insertNode(self):
+        # linked list after inserting value 2 at position 1:
+        # [2] -> [1]
+        self.testSingleLinkedList.insertNode(2, 1)
+        self.assertEqual(self.testSingleLinkedList.returnValue(1), 2)
+        self.assertEqual(self.testSingleLinkedList.returnValue(2), 1)
+
+        # linked list after inserting value 4 at position 20:
+        # [2] -> [1] -> [4]
+        # since 20 is larger than amount of current nodes, will append to end
+        self.testSingleLinkedList.insertNode(4, 20)
+        self.assertEqual(self.testSingleLinkedList.returnValue(3), 4)
+
+        # linked list after inserting value 6 at position 2:
+        # [2] -> [6] -> [1] -> [4]
+        self.testSingleLinkedList.insertNode(6, 2)
+        self.assertEqual(self.testSingleLinkedList.returnValue(2), 6)
+        self.assertEqual(self.testSingleLinkedList.returnValue(3), 1)
+        self.assertEqual(self.testSingleLinkedList.returnValue(4), 4)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 4)
+
+        # inserting a zero should result in error
+        self.testSingleLinkedList.insertNode(10, 0)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 4)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 2 ]-->[ 2 | 6 ]-->[ 3 | 1 ]-->[ 4 | 4 ]-->X")
+
+        # inserting a negative should result in error
+        self.testSingleLinkedList.insertNode(10, -1)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 4)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 2 ]-->[ 2 | 6 ]-->[ 3 | 1 ]-->[ 4 | 4 ]-->X")
+
+    def test_popLinkedList(self):
+        self.testSingleLinkedList.appendNode(2)
+        self.testSingleLinkedList.appendNode(3)
+
+        self.assertEqual(self.testSingleLinkedList.popLinkedList(), 3)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 1 ]-->[ 2 | 2 ]-->X")
+
+    def test_deleteNode(self):
+        for k in range(2, 5 + 1):
+            self.testSingleLinkedList.appendNode(k)
+
+        # linked list looks like:
+        # [1] -> [2] -> [3] -> [4] -> [5] -> X
+
+        # cannot delete not existent node
+        self.testSingleLinkedList.deleteNode(20)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 5)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 1 ]-->[ 2 | 2 ]-->[ 3 | 3 ]-->[ 4 | 4 ]-->[ 5 | 5 ]-->X")
+
+        self.testSingleLinkedList.deleteNode(0)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 5)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 1 ]-->[ 2 | 2 ]-->[ 3 | 3 ]-->[ 4 | 4 ]-->[ 5 | 5 ]-->X")
+
+        self.testSingleLinkedList.deleteNode(-1)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 5)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 1 ]-->[ 2 | 2 ]-->[ 3 | 3 ]-->[ 4 | 4 ]-->[ 5 | 5 ]-->X")
+
+        # linked list looks like:
+        # [1] -> [2] -> [4] -> [5] -> X
+        self.testSingleLinkedList.deleteNode(3)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 4)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 1 ]-->[ 2 | 2 ]-->[ 3 | 4 ]-->[ 4 | 5 ]-->X")
+
+        # linked list looks like:
+        # [2] -> [4] -> [5] -> X
+        self.testSingleLinkedList.deleteNode(1)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 3)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 2 ]-->[ 2 | 4 ]-->[ 3 | 5 ]-->X")
+
+        # linked list looks like:
+        # [2] -> [4] -> X
+        self.testSingleLinkedList.deleteNode(self.testSingleLinkedList.nNodes)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 2)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 2 ]-->[ 2 | 4 ]-->X")
+
+    def test_deleteValue(self):
+        for k in range(2, 5 + 1):
+            if k % 2 == 0:
+                self.testSingleLinkedList.append(2)
+            else:
+                self.testSingleLinkedList.append(1)
+
+        # linked list looks like:
+        # [1] -> [2] -> [1] -> [2] -> [1] -> [2] -> X
+        self.testSingleLinkedList.deleteValue(3)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 6)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 1 ]-->[ 2 | 2 ]-->[ 3 | 1 ]-->[ 4 | 2 ]-->[ 5 | 1 ]-->[ 6 | 2 ]-->X")
+
+        # linked list looks like:
+        # [1] -> [1] -> [1] -> X
+        self.testSingleLinkedList.deleteValue(3)
+        self.assertEqual(self.testSingleLinkedList.nNodes, 3)
+        self.assertEqual(self.testSingleLinkedList.printLinkedList(), "[ 1 | 1 ]-->[ 2 | 1 ]-->[ 3 | 1 ]-->X")
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
